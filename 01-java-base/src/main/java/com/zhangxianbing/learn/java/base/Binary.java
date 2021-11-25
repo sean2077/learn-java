83dec0c02f69c244fc942a2fef571e1d1067ee68
zhangxianbing <zhangxianbing09@163.com>
2021-11-25 17:02:33 +0800

    style: use idea build-in style

--- 01-java-base/src/main/java/com/zhangxianbing/learn/java/base/Binary.java
+++ 01-java-base/src/main/java/com/zhangxianbing/learn/java/base/Binary.java
@@ -1,18 +1,22 @@
 package com.zhangxianbing.learn.java.base;
 
 public class Binary {
-  public static void main(String[] args) {
-    int i = 5;
-    int j = 10;
-    System.out.println(i + ~j);
+    public static void main(String[] args) {
+        int i = 5;
+        int j = 10;
+        System.out.println(i + ~j);
 
-    int[] arr = new int[] {3, -2};
-    for (int a : arr) {
-      System.out.println(a);
-      System.out.println(~a);
-      System.out.println(-a);
-      System.out.println(-a == ~a + 1);
-      System.out.println(~a == -a - 1);
+        int[] arr = new int[]{3, -2};
+        for (int a : arr) {
+            // 原数
+            System.out.println("a:" + a + "  二进制:" + Integer.toBinaryString(a));
+            // 按位取反
+            System.out.println("~a:" + ~a + "  二进制:" + Integer.toBinaryString(~a));
+            // 相反数
+            System.out.println("-a:" + -a + "  二进制:" + Integer.toBinaryString(-a));
+
+            System.out.println(-a == ~a + 1);
+            System.out.println(~a == -a - 1);
+        }
     }
-  }
 }
---
+++ 01-java-base/src/main/java/com/zhangxianbing/learn/java/base/datatype/Boxing.java
@@ -0,0 +1,7 @@
+package com.zhangxianbing.learn.java.base.datatype;
+
+/**
+ * 装箱与拆箱
+ */
+public class Boxing {
+}
